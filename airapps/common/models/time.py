83f10f1fbaaffd5ff4aba90cb88e872bf9c24065
jwzzang <jinooooo@jinooooo.local>
2023-05-23 18:32:01 +0900

    create TimeModel

---
+++ airapps/common/models/time.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(
+        verbose_name="생성일",
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        verbose_name="수정일",
+        auto_now=True,
+    )
+
+    class Meta:
+        abstract = True
