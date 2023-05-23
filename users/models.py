10f8e196d38c614ce8af535e497649fcb5f93846
jwzzang <jinooooo@jinooooo.local>
2023-05-23 18:53:09 +0900

    Create CustomUserModel

--- users/models.py
+++ users/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = ("male", "Male")
+        FEMALE = ("female", "Female")
+
+    class LanguageChoices(models.TextChoices):
+        KR = ("kr", "Korean")
+        EN = ("en", "English")
+
+    class CurrencyChoices(models.TextChoices):
+        WON = ("won", "Korean Won")
+        USD = ("usd", "Dollar")
+
+    name = models.CharField(
+        verbose_name="이름",
+        max_length=20,
+    )
+    avatar = models.ImageField(
+        verbose_name="프로필 사진",
+        upload_to="image/",
+        blank=True,
+    )
+    is_host = models.BooleanField(
+        verbose_name="호스트",
+        default=False
+    )
+    gender = models.CharField(
+        verbose_name="성별",
+        max_length=10,
+        choices=GenderChoices.choices,
+    )
+    language = models.CharField(
+        verbose_name="언어",
+        max_length=10,
+        choices=LanguageChoices.choices,
+    )
+    currency = models.CharField(
+        verbose_name="화폐",
+        max_length=20,
+        choices=CurrencyChoices.choices,
+    )
