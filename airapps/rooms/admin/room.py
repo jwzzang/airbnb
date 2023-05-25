e8042de1bf518a22b44135bf3937e6fdf68d81b2
jwzzang <jinooooo@jinooooo.local>
2023-05-25 15:06:42 +0900

    Create RoomAdmin

---
+++ airapps/rooms/admin/room.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from airapps.rooms.models import Room, Amenity
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "price",
+        "kind",
+        "owner",
+        "created_at",
+    )
+
+    list_filter = (
+        "country",
+        "city",
+        "pet_friendly",
+        "kind",
+        "amenities",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = (
+        "name",
+        "^price",
+        "=owner__username",
+    )
+
+
+@admin.register(Amenity)
+class AmenityAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "description",
+        "created_at",
+        "updated_at",
+    )
