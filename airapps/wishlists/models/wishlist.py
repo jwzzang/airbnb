cbe1760b79bb92e540db96f9009182502383c788
jwzzang <jinooooo@jinooooo.local>
2023-05-25 16:41:22 +0900

    Created WishlistModel

---
+++ airapps/wishlists/models/wishlist.py
@@ -0,0 +1,28 @@
+from django.db import models
+from airapps.common.models import TimeStampedModel
+
+
+class Wishlist(TimeStampedModel):
+    name = models.CharField(
+        verbose_name="목록",
+        max_length=150,
+    )
+    rooms = models.ManyToManyField(
+        "rooms.Room",
+        verbose_name="숙소",
+        related_name="wishlists",
+    )
+    experiences = models.ManyToManyField(
+        "experiences.Experience",
+        verbose_name="체험",
+        related_name="wishlists",
+    )
+    user = models.ForeignKey(
+        "users.User",
+        verbose_name="주인",
+        on_delete=models.CASCADE,
+        related_name="wishlists",
+    )
+
+    def __str__(self):
+        return self.name
